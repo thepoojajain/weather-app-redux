57d0af5d4f6c3fea312463fb269316620825ff9b
thepoojajain <pooja.jain133@gmail.com>
2022-06-27 09:32:16 +0530

    2 files modified

--- src/UVRays/UVRays.js
+++ src/UVRays/UVRays.js
@@ -2,5 +2,18 @@ import React from 'react';
 import './UVRays.css';
 
 export default function UVRays({ weatherData }) {
-  return <h1>UVRays</h1>;
+  return (
+    <div classNmae="card">
+      <div>
+        <img
+          src="https://img.freepik.com/free-vector/sun-icon-bright-yellow-sol-symbol-with-rays-childish-simple-style_53562-14613.jpg?"
+          alt="Sun-Image"
+          className="sun-icon"
+        />
+      </div>
+      <div>
+        <h1>{weatherData.current.uvi} UVI</h1>
+      </div>
+    </div>
+  );
 }
