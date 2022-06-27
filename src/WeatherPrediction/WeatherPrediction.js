a0b9db708ffdbac204dcff0ba74dadcc7645099e
thepoojajain <pooja.jain133@gmail.com>
2022-06-27 10:27:02 +0530

    2 files modified, 2 added

--- src/WeatherPrediction/WeatherPrediction.js
+++ src/WeatherPrediction/WeatherPrediction.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import './WeatherPrediction.css';
+import '../WeatherPredictionCard/WeatherPredictionCard';
 
 export default function WeatherPrediction({ weatherData }) {
   return (
     <div>
       <h2>Weather Prediction</h2>
-      <div className="WeatherCard">
-        <div>
-          <img
-            src="https://cdn-icons-png.flaticon.com/512/1247/1247109.png"
-            alt="cloud-icon"
-            className="cloud-icon"
-          />
-        </div>
-        <div className="cardDate">
-          <p>{weatherData.daily[0].dt}</p>
-          <div className="cardTemp">
-            <h3>{weatherData.daily[0].weather[0].description}</h3>
-            <h3 className="minMaxTemp">
-              {weatherData.daily[0].temp.max.toFixed()}
-              <span>&deg;</span> / {weatherData.daily[0].temp.min.toFixed()}
-              <span>&deg;</span>
-            </h3>
-          </div>
-        </div>
-      </div>
+      <WeatherPredictionCard weatherData={weatherData} />
     </div>
   );
 }
